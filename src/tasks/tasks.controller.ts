5074692c30cf57853f741e77963682666511483e
Alexis Lopez <alg9105@gmail.com>
2020-05-23 14:24:06 -0700

    Get All Tasks and Create Task created.

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+
+@Controller("tasks")
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body("title") title: string,
+    @Body("description") description: string
+  ) {
+    return this.tasksService.createTask(title, description);
+  }
+}
